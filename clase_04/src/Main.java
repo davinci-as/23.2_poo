f7c8a768141f4e336c23e1dc71c544291012d03a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-30 15:00:32 -0300

    feat: add SilentCat class

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -17,12 +17,24 @@ class Cat  {
     public void setColor(int color) {
         this.color = color;
     }
+
+    public String jump() {
+        return "juuumping";
+    }
+
+}
+
+class SilentCat extends Cat {
+    public String jump() {
+        return "....";
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Cat miMichi = new Cat();
+        SilentCat miMichi = new SilentCat();
         miMichi.setAge(6);
         miMichi.setColor(1);
+        System.out.println(miMichi.jump());
     }
 }
