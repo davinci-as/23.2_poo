ab4b891e1541b2e62776b3848e756c71ab4ef2ad
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-30 15:17:44 -0300

    feat: add abstract method jump

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -1,6 +1,7 @@
-class Cat  {
+abstract class Cat  {
     private int age;
     private int color;
+    private int energy;
 
     public int getAge() {
         return age;
@@ -18,23 +19,42 @@ class Cat  {
         this.color = color;
     }
 
-    public String jump() {
-        return "juuumping";
+    public abstract String jump();
+
+    public int getEnergy() {
+        return energy;
     }
 
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
 }
 
 class SilentCat extends Cat {
     public String jump() {
+        if(getEnergy() >= 10) return "Tired";
+        setEnergy(getEnergy() + 1);
         return "....";
+    }
+}
+
+class WildCat extends Cat {
+    public String jump() {
+        if(getEnergy() >= 10) return "Tired";
+        setEnergy(getEnergy() + 5);
+        return "rrrrrr";
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         SilentCat miMichi = new SilentCat();
+        miMichi.setEnergy(1);
         miMichi.setAge(6);
         miMichi.setColor(1);
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
+        System.out.println(miMichi.jump());
         System.out.println(miMichi.jump());
     }
 }
