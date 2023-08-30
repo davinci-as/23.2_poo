d0ede03c2491279ff9baf487090a1e9cb00e2cfc
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-30 14:41:06 -0300

    feat: add Cat class

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -1,5 +1,28 @@
+class Cat  {
+    private int age;
+    private int color;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cat miMichi = new Cat();
+        miMichi.setAge(6);
+        miMichi.setColor(1);
     }
 }
