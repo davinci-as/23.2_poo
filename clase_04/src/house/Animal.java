4b1fedda7c9811cfb3b29faccdba15321f6baf5c
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-31 14:58:32 -0300

    refactor: move Cat members to Animal class

---
+++ clase_04/src/house/Animal.java
@@ -0,0 +1,31 @@
+package house;
+
+public class Animal {
+    private int age;
+    private int color;
+    private int energy;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+}
