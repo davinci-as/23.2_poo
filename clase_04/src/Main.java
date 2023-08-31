2d9657a2ea0f467065624e8dc6b5160031c05727
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-08-31 15:05:07 -0300

    feat: add bolaDeNueve and some training routine

--- clase_04/src/Main.java
+++ clase_04/src/Main.java
@@ -3,13 +3,24 @@ import house.WildCat;
 
 public class Main {
     public static void main(String[] args) {
+        SilentCat bolaDeNieve = new SilentCat();
         WildCat miMichi = new WildCat();
+
+        bolaDeNieve.setEnergy(1);
+        bolaDeNieve.setAge(6);
+        bolaDeNieve.setColor(1);
+
         miMichi.setEnergy(1);
         miMichi.setAge(6);
         miMichi.setColor(1);
-        System.out.println(miMichi.jump());
-        System.out.println(miMichi.jump());
-        System.out.println(miMichi.jump());
-        System.out.println(miMichi.jump());
+
+        System.out.println("Rutina de entrenamiento\n");
+        for (int i = 0; i < 4; i++) {
+            bolaDeNieve.jump();
+            miMichi.jump();
+        }
+
+        System.out.println(miMichi.getClass().getName() + " " + miMichi.jump());
+        System.out.println(bolaDeNieve.getClass().getName() + " " + bolaDeNieve.jump());
     }
 }
