02818cb2cf971e4bccfd1d14e47e1b0059dc4e36
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-02 15:19:52 -0300

    feat: add clase_12 template

---
+++ clase_12/src/estadisticas/Player.java
@@ -0,0 +1,49 @@
+package estadisticas;
+
+public class Player {
+    private String name;
+    private int number;
+    private String position;
+    private int goalCounter;
+    private Team team;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getGoalCounter() {
+        return goalCounter;
+    }
+
+    public void setGoalCounter(int goalCounter) {
+        this.goalCounter = goalCounter;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
