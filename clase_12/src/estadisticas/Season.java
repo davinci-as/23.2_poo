5e3fac152c19c361d2816b1eb9d25c00d67c6474
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-02 15:31:38 -0300

    feat: add Season class

---
+++ clase_12/src/estadisticas/Season.java
@@ -0,0 +1,49 @@
+package estadisticas;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Season {
+    private String name;
+    private Date startDate;
+    private Date finishDate;
+    private ArrayList<Team> teams;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public void addTeam(Team team){
+    }
+    public void removeTeam(Team team){
+    }
+
+    public ArrayList<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(ArrayList<Team> teams) {
+        this.teams = teams;
+    }
+}
