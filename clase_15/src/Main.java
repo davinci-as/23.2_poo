215b8afb1f8674a7bf7c114e6c4f8dc5cd0e6b0a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-23 15:18:27 -0300

    feat: add new team

--- clase_15/src/Main.java
+++ clase_15/src/Main.java
@@ -1,12 +1,18 @@
 import estadisticas.Player;
+import estadisticas.Team;
 
 public class Main {
     public static void main(String[] args) {
+        Team argentina = new Team();
+        argentina.setName("Argentina");
+        argentina.setGoalCounter(0);
+        argentina.setTableScore(0);
+
         Player messi = new Player();
         messi.setName("Lionel Messi");
         messi.setNumber(10);
         messi.setPosition("Delantero");
         messi.setGoalCounter(0);
-
+        messi.setTeam(argentina);
     }
 }
