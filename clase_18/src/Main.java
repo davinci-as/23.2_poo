4873ceb6dfb6a4e08f244444fe21c2dec1962495
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-02 14:19:27 -0300

    feat: add new player

--- clase_18/src/Main.java
+++ clase_18/src/Main.java
@@ -7,9 +7,25 @@ public class Main {
 
         Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
         Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
+        Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
 
-        messi.getTeam().getPlayers().add(messi);
-        dimaria.getTeam().getPlayers().add(dimaria);
+        // player -> agregar equipo
+        messi
+            .getTeam()    //Team
+            .getPlayers() //ArrayList<Player>
+            .add(messi);  //add(Player)
+
+        dimaria
+                .getTeam()    //Team
+                .getPlayers() //ArrayList<Player>
+                .add(dimaria);  //add(Player)
+
+        dibu
+                .getTeam()    //Team
+                .getPlayers() //ArrayList<Player>
+                .add(dibu);  //add(Player)
+
+
 
         System.out.println(messi.getTeam().getName());
     }
