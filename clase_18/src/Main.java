be25e23efb17b8fdbdbfbada808fe2fc8f587757
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-02 14:32:58 -0300

    refactor: update player names

--- clase_18/src/Main.java
+++ clase_18/src/Main.java
@@ -5,23 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 4, 3);
 
-        Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
+        Player fideo = new Player("Angel Di Maria", 7, "Delantero", argentina);
         Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
         Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
 
         messi.addPlayerToTeamList(messi);
-
-        dimaria
-                .getTeam()    //Team
-                .getPlayers() //ArrayList<Player>
-                .add(dimaria);  //add(Player)
-
-        dibu
-                .getTeam()    //Team
-                .getPlayers() //ArrayList<Player>
-                .add(dibu);  //add(Player)
-
-
+        fideo.addPlayerToTeamList(fideo);
+        dibu.addPlayerToTeamList(dibu);
 
         System.out.println(messi.getTeam().getName());
     }
