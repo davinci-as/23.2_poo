f62be02bead8ccd58f336db1eaeab9a98d43a22d
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-06 14:28:56 -0300

    feat: add new team

---
+++ clase_19/src/Main.java
@@ -0,0 +1,20 @@
+import estadisticas.Player;
+import estadisticas.Team;
+
+public class Main {
+    public static void main(String[] args) {
+        Team argentina = new Team("Argentina", 4, 3);
+        Team francia = new Team("Francia", 0, 0);
+
+
+        Player fideo = new Player("Angel Di Maria", 7, "Delantero", argentina);
+        Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
+        Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
+
+        Player griezmann = new Player("antoine griezmann", 7, "Delantero", francia);
+        Player mbbappe = new Player("Kylian mbappe", 10, "Delantero", francia);
+        Player lloris = new Player("Hugo lloris", 1, "Arquero", francia);
+
+        argentina.showPlayersList();
+    }
+}
