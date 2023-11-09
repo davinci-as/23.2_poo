b90fd9945db4138968901e73b68c785e5efde5dd
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-09 15:07:33 -0300

    feat: generate Arg player

--- clase_20/src/Main.java
+++ clase_20/src/Main.java
@@ -3,10 +3,38 @@ import estadisticas.Player;
 import estadisticas.Season;
 import estadisticas.Team;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static String[][] generateArgPlayer() {
+        String[][] jugadoresArgentina = new String[10][2];
+
+        // Asignar los nombres de los jugadores
+        jugadoresArgentina[0][0] = "Lionel Messi";
+        jugadoresArgentina[0][1] = "Delantero";
+
+        jugadoresArgentina[1][0] = "Sergio Agüero";
+        jugadoresArgentina[1][1] = "Delantero";
+
+        jugadoresArgentina[2][0] = "Paulo Dybala";
+        jugadoresArgentina[2][1] = "Delantero";
+
+        jugadoresArgentina[3][0] = "Angel Di Maria";
+        jugadoresArgentina[3][1] = "Delantero";
+
+        jugadoresArgentina[4][0] = "Gonzalo Higuaín";
+        jugadoresArgentina[5][0] = "Nicolas Otamendi";
+
+        jugadoresArgentina[6][0] = "Marcos Rojo";
+        jugadoresArgentina[7][0] = "Leandro Paredes";
+        jugadoresArgentina[8][0] = "Lucas Ocampos";
+        jugadoresArgentina[9][0] = "Nicolás Tagliafico";
+        jugadoresArgentina[9][1] = JOptionPane.showInputDialog("por favor ingrese la posición de: " + jugadoresArgentina[9][0]);
+
+        return jugadoresArgentina;
+    }
     public static void main(String[] args) {
         Season faseGrupoMundial = new Season();
         Team argentina = new Team("Argentina", 4, 3);
@@ -15,13 +43,11 @@ public class Main {
         faseGrupoMundial.addTeam(argentina);
         faseGrupoMundial.addTeam(francia);
 
-        new Player("Emiliano Martinez", 1, "Arquero", argentina);
-        new Player("Angel Di Maria", 7, "Delantero", argentina);
-        new Player("Lionel Messi", 10, "Delantero", argentina);
+        String[][] jugadoresArgentina = generateArgPlayer();
 
-        new Player("antoine griezmann", 7, "Delantero", francia);
-        new Player("Kylian mbappe", 10, "Delantero", francia);
-        new Player("Hugo lloris", 1, "Arquero", francia);
+        for (int i = 0; i < jugadoresArgentina.length; i++) {
+            new Player(jugadoresArgentina[i][0], i + 1, jugadoresArgentina[i][1], argentina);
+        }
 
         argentina.showPlayersList();
 
