8b14944daa73589ec4047d5e61c7fdd6ef59cc6a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-09 14:42:31 -0300

    feat: add clase_20

---
+++ clase_20/src/estadisticas/Team.java
@@ -0,0 +1,75 @@
+package estadisticas;
+
+import java.util.ArrayList;
+
+public class Team {
+    private String name;
+    private ArrayList<Player> players;
+    private int tableScore;
+    private int goalCounter;
+
+    public Team() {
+        this.players = new ArrayList<Player>();
+    }
+
+    public Team(String name) {
+        this.players = new ArrayList<Player>();
+        this.setName(name);
+    }
+
+    public Team(String name, int goalCounter, int tableScore) {
+        this.players = new ArrayList<Player>();
+        this.setName(name);
+        this.setGoalCounter(goalCounter);
+        this.setTableScore(tableScore);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player player) {
+        getPlayers().add(player);
+    }
+
+    public void removePlayer(Player player) {
+        getPlayers().remove(player);
+    }
+
+    public int getTableScore() {
+        return tableScore;
+    }
+
+    public void setTableScore(int tableScore) {
+        this.tableScore = tableScore;
+    }
+
+    public int getGoalCounter() {
+        return goalCounter;
+    }
+
+    public void setGoalCounter(int goalCounter) {
+        this.goalCounter = goalCounter;
+    }
+
+    public void showPlayersList() {
+        int lenght = this.getPlayers().size();
+        String message = "En %s hay %d jugadores\n";
+        System.out.println(
+            String.format(message,getName(),lenght)
+        );
+        getPlayers().forEach(player -> {
+            String playerText = player.getName() + " " + player.getGoalCounter();
+            System.out.println(playerText);
+        });
+    }
+}
