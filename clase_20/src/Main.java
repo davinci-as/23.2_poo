4622e33f76adcb128d49967b03af692861826cbf
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-11-09 15:14:56 -0300

    feat: get player info from user input

--- clase_20/src/Main.java
+++ clase_20/src/Main.java
@@ -9,29 +9,12 @@ import java.util.List;
 
 public class Main {
     public static String[][] generateArgPlayer() {
-        String[][] jugadoresArgentina = new String[10][2];
+        String[][] jugadoresArgentina = new String[2][2];
 
-        // Asignar los nombres de los jugadores
-        jugadoresArgentina[0][0] = "Lionel Messi";
-        jugadoresArgentina[0][1] = "Delantero";
-
-        jugadoresArgentina[1][0] = "Sergio Agüero";
-        jugadoresArgentina[1][1] = "Delantero";
-
-        jugadoresArgentina[2][0] = "Paulo Dybala";
-        jugadoresArgentina[2][1] = "Delantero";
-
-        jugadoresArgentina[3][0] = "Angel Di Maria";
-        jugadoresArgentina[3][1] = "Delantero";
-
-        jugadoresArgentina[4][0] = "Gonzalo Higuaín";
-        jugadoresArgentina[5][0] = "Nicolas Otamendi";
-
-        jugadoresArgentina[6][0] = "Marcos Rojo";
-        jugadoresArgentina[7][0] = "Leandro Paredes";
-        jugadoresArgentina[8][0] = "Lucas Ocampos";
-        jugadoresArgentina[9][0] = "Nicolás Tagliafico";
-        jugadoresArgentina[9][1] = JOptionPane.showInputDialog("por favor ingrese la posición de: " + jugadoresArgentina[9][0]);
+        for (int i = 0; i < jugadoresArgentina.length; i++) {
+            jugadoresArgentina[i][0] = JOptionPane.showInputDialog("por favor el nombre del jugador que usa la camiseta: " + (i+1));
+            jugadoresArgentina[i][1] = JOptionPane.showInputDialog("por favor ingrese la posición de: " + jugadoresArgentina[i][0]);
+        }
 
         return jugadoresArgentina;
     }
